01b017e5be27486e52190a383074fa5825961eb3
Barbara <barbi.medica@gmail.com>
2025-10-28 12:00:57 +0100

    Inicijalni commit-2

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express(); // u varijablu app pohranjujemo objekt koji predstavlja Express
 //aplikaciju
-const PORT = 3000; // port na kojem će poslužitelj slušati zahtjeve
+const PORT = 3005; // port na kojem će poslužitelj slušati zahtjeve
+
+
+let korisnici = [
+  { id: 1, ime: 'Ivan', prezime: 'Ivić' },
+  { id: 2, ime: 'Ana', prezime: 'Anić' },
+  { id: 3, ime: 'Marko', prezime: 'Markić' }
+];
+const path = require('path');
+
 app.get('/', (req, res) => {
 res.send('Hello, world!');
 });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/korisnici', (req, res) => {   
+  
+  res.json(korisnici);
+});
+
 app.listen(PORT, error => {
 if (error) {
 console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
