ad75182957e39437ffd6e61672dd162ba2aec884
Barbara <barbi.medica@gmail.com>
2025-10-29 11:15:16 +0100

    2.commit

--- index.js
+++ index.js
@@ -3,28 +3,45 @@ const app = express(); // u varijablu app pohranjujemo objekt koji predstavlja E
 //aplikaciju
 const PORT = 3005; // port na kojem će poslužitelj slušati zahtjeve
 
+const path = require('path');
 
 let korisnici = [
   { id: 1, ime: 'Ivan', prezime: 'Ivić' },
   { id: 2, ime: 'Ana', prezime: 'Anić' },
   { id: 3, ime: 'Marko', prezime: 'Markić' }
 ];
-const path = require('path');
 
-app.get('/', (req, res) => {
-res.send('Hello, world!');
-});
+
+//app.get('/', (req, res) => {
+//res.send('Hello, world!');
+//});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.get('/korisnici', (req, res) => {   
-  
-  res.json(korisnici);
+   res.json(korisnici);
 });
+
+app.get('/korisnici/:ime', (req, res) => {
+  const ime = req.params.ime;
+  const korisnik = korisnici.find(k => 
+    k.ime.toLowerCase() === ime.toLowerCase()
+  );
+
+  if (korisnik) {
+    res.json(korisnik);
+  } else {
+    res.status(404).json({ poruka: 'Korisnik nije pronađen.' });
+  }
+});
+
+
 
 app.listen(PORT, error => {
 if (error) {
